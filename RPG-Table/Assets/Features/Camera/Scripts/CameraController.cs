ac38520bd4d0527c702e7c4425c84f24f686aa25
Mikołaj Musioł <96127814+mikolajmusiol@users.noreply.github.com>
2025-04-22 22:49:37 +0200

    Implemented basic camera controls (#22)

---
+++ RPG-Table/Assets/Features/Camera/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+using Unity.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 50f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public float scrollSpeed = 50f;
+    public float minY = 20f;
+    public float maxY = 500f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+
+        transform.position = pos;
+    }
+}
