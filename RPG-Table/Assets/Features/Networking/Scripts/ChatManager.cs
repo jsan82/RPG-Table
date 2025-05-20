2f23b2bf0c931be0c597be1860704916e3daa6a3
yiro0 <bartmat.p@gmail.com>
2025-05-20 08:45:40 +0200

    Implementing basic multiplayer connetion #5
    Progress if any

---
+++ RPG-Table/Assets/Features/Networking/Scripts/ChatManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using TMPro;
+
+public class ChatManager : NetworkBehaviour
+{
+    [SerializeField] ChatMessage chatMessagePrefab;
+    [SerializeField] CanvasGroup chatContent;
+    [SerializeField] TMP_InputField chatInput;
+    
+    public static ChatManager Singleton; 
+    public string playerName;
+
+    void Awake()
+    {
+        ChatManager.Singleton = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SendChatMessage(chatInput.text, playerName);
+            chatInput.text = "";
+        }
+    }
+
+    // yiro0 modify _fromWho = null
+    public void SendChatMessage(string _message, string _fromWho = null)
+    {
+        if (string.IsNullOrWhiteSpace(_message)) return;
+        string S = _fromWho + ": " + _message;
+        SendChatMessageServerRpc(S);
+    }
+
+    void AddMessage(string msg)
+    {
+        ChatMessage CM = Instantiate(chatMessagePrefab, chatContent.transform);
+        CM.SetText(msg);
+    }
+    
+    [ServerRpc(RequireOwnership = false)]
+    void SendChatMessageServerRpc(string message)
+    {
+        ReceiveChatMessageClientRpc(message);
+    }
+
+    [ClientRpc]
+    void ReceiveChatMessageClientRpc(string message)
+    {
+        ChatManager.Singleton.AddMessage(message);
+    }
+}
