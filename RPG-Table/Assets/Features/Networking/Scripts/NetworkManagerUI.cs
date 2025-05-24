c6ff1f75e5d100469380cdb8b66955bec4d1d5b6
yiro0 <bartmat.p@gmail.com>
2025-05-25 00:31:30 +0200

    Rolled back network manager.
    Simple connection is available. Just build.
    More will be done on top of this. Finally figured out how to do this in simple way so that we can build on this in future.
    Just spam prefabs, and add prefabs in prefabs list in NetworkPrefabs.asset Chaning UI simple aswell. Just drag'n'drop.
    Player prefab cylinder is only for "debug" purposes.

---
+++ RPG-Table/Assets/Features/Networking/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private GameObject serverButton;
+    [SerializeField] private GameObject hostButton;
+    [SerializeField] private GameObject clientButton;
+
+    private void Awake()
+    {
+        serverButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                NetworkManager.Singleton.StartServer();
+            }
+        );
+        
+        hostButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+        );
+        
+        clientButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+        );
+    }
+}
