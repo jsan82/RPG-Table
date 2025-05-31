28a350122c4bd542d476e2884ba71104976af659
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2025-05-31 23:35:08 +0200

    audio branch

---
+++ RPG-Table/Assets/Features/Audio/Scripts/SFXManager.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource _source;
+    [SerializeField] private AudioSource _loopedSource;
+    [SerializeField] private SFXElement[] _elements;
+
+    private Dictionary<SFXType, AudioClip> _sfxDict = new();
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        foreach (var element in _elements)
+        {
+            _sfxDict.Add(element.Type, element.Clip);
+        }
+    }
+
+    public void Play(SFXType type)
+    {
+        var clip = _sfxDict[type];
+        if (clip != null)
+        {
+            _source.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayLooped()
+    {
+        _loopedSource.Play();
+    }
+
+    public void StopLooped()
+    {
+        _loopedSource.Stop();
+    }
+}
